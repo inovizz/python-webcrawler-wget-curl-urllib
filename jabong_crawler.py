b1b0af878519c9bffcb444ca82ba1f9a362310a4
mounarajan <mun_india@yahoo.co.in>
2015-04-07 10:53:47 +0530

    Added function to extract product urls

--- jabong_crawler.py
+++ jabong_crawler.py
@@ -1,6 +1,7 @@
 import urllib.request
 import os
 import re
+import time
 
 class LibAccess(object):
 	#Retrive Seed URL to crawl(Either spidering or content extraction) via constructor method
@@ -13,7 +14,7 @@ class LibAccess(object):
 		self.wget_data = os.popen('wget -qO- %s'% url).read()
 		return self.wget_data
 
-    #Downloading webpage content via curl
+	#Downloading webpage content via curl
 	def curl(self):
 		url = self.url
 		self.curl_data = os.popen('curl -I %s'% url).read()
@@ -27,29 +28,107 @@ class LibAccess(object):
 
 #To retrive all the urls from site(Spidering)
 class Spidering(LibAccess):
-	def category1(self):
-		f1 = open('exampleFile.txt','w')
+	def category1(self,sleep):
+		f1 = open('category_urls.txt','w')
 		spider = self.wget_data
-		#print (spider)
-		#f1.write(spider)
 		if re.search(r'<a href\=\"[^\"]*\"\sid\=\"qa\-main',spider):
-		    main_url = re.findall(r'<a href\=\"([^\"]*)\"\sid\=\"qa\-main',spider)
-		    for url in main_url:
-		    	#pass
-			    print (url)
-			    f1.write(url+"\n")
+			main_url = re.findall(r'<a href\=\"([^\"]*)\"\sid\=\"qa\-main',spider)
+			for url in main_url:
+				#pass
+				print (url)
+				f1.write(url+"\n")
 		else:
 			print ("Match Not Found")
+		time.sleep(sleep)
 		f1.close()
 
+	#Jabong has all category urls via AJAX call this function will extract all category urls
+	def ajaxCategories(self,sleep):
+		f1 = open('category_urls.txt','a')
+		ajaxURL = "http://www.jabong.com/index/topnav/?e_100"
+		scrap.wget(ajaxURL)
+		spider = self.wget_data
+		if re.search(r'topnav\-title\\"\><a href\=\\\"[^\"]*\"',spider):
+			main_url = re.findall(r'topnav\-title\\"\><a href\=\\\"([^\"]*)\"',spider)
+			main_url = re.findall(r'<a href\=\\\"([^\"]*)\"\>',spider)
+			#re.sub(r"\\","",main_url)
+			for url in main_url:
+				#pass
+				#url = "r%s"%url
+				url = re.sub(r"\\","",url)
+				if url in "^http":
+					f1.write(url+"\n")
+				else:
+					url = re.sub(r"^","http://www.jabong.com",url)
+					f1.write(url+"\n")
+				#url = re.sub(r"^","http://www.jabong.com",url)
+				print (url)
+		else:
+			print ("Match Not Found")
+		time.sleep(sleep)
+		f1.close()
+
+	def pagination(self,sleep):
+		f1 = open('pagination_urls.txt','w')
+		ajaxURL = "http://www.jabong.com/index/topnav/?e_100"
+		scrap.wget(ajaxURL)
+		spider = self.wget_data
+		if re.search(r'topnav\-title\\"\><a href\=\\\"[^\"]*\"',spider):
+			main_url = re.findall(r'topnav\-title\\"\><a href\=\\\"([^\"]*)\"',spider)
+			main_url = re.findall(r'<a href\=\\\"([^\"]*)\"\>',spider)
+			#re.sub(r"\\","",main_url)
+		for url in main_url:
+			#pass
+			#url = "r%s"%url
+			url = re.sub(r"\\","",url)
+			if url in "^http":
+				f1.write(url+"\n")
+			else:
+				url = re.sub(r"^","http://www.jabong.com",url)
+				f1.write(url+"\n")
+				#url = re.sub(r"^","http://www.jabong.com",url)
+			print (url)
+		else:
+			print ("Match Not Found")
+		time.sleep(sleep)
+		f1.close()
+
+	def product(self,sleep):
+		f1 = open('category_urls.txt','r')
+		f2 = open('product_urls.txt','a')
+		for categoryurls in f1:
+			ajaxURL = categoryurls
+			scrap.wget(ajaxURL)
+			spider = self.wget_data
+			if re.search(r'unbxdparam\_sku\=\"[^\"]+\"\s*href\=\"[^\"]*\"',spider):
+				main_url = re.findall(r'unbxdparam\_sku\=\"[^\"]+\"\s*href\=\"([^\"]*)\"',spider)
+				#re.sub(r"\\","",main_url)
+				for url in main_url:
+					#pass
+					#url = "r%s"%url
+					url = re.sub(r"\\","",url)
+					if url in "^http":
+						f2.write(url+"\n")
+					else:
+					#	url = re.sub(r"^","http://www.jabong.com",url)
+						f2.write(url+"\n")
+					#url = re.sub(r"^","http://www.jabong.com",url)
+					print (url)
+			else:
+				print ("Match Not Found")
+			time.sleep(sleep)
 
 
 #To extract the content from product pages with the extracted category urls
 #class ContetExtraction(object):		
 		
 
+#sleep time
+sleep = 10
 #Creating objects via class
 scrap = Spidering()
 #Give seed url as parameter as it is compulsary to crawl the webpage
 scrap.wget("http://jabong.com")
-scrap.category1()
+#scrap.category1(sleep)
+#scrap.ajaxCategories(sleep)
+scrap.product(sleep)
